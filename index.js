0a4d70f762072054fe497a8f8e6c578d6323ce9a
juanf1586 <166897516+juanf1586@users.noreply.github.com>
2024-05-27 04:51:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -58,3 +58,51 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
+//------------------------------ boton de los cards
+document.addEventListener('DOMContentLoaded', function() {
+    var cards = document.querySelectorAll('.card');
+    
+    cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            var link = card.getAttribute('data-link');
+           
+            if (link=="dental.html") {
+                window.location.href = link;
+            }else if (link=="rinones.html") {
+                window.location.href = link;
+            }else if (link=="nariz.html") {
+                window.location.href = link;
+            }else if (link=="rodilla.html") {
+                window.location.href = link;
+            }else if (link=="lipo.html") {
+                window.location.href = link;
+            }
+        });
+    });
+});
+
+
+
+//-------drop de menu
+
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    
+    // Cerrar el menú cuando se hace clic en un enlace dentro del menú desplegable de servicios
+    document.querySelectorAll('.dropdown-item').forEach(function(dropdownItem) {
+        dropdownItem.addEventListener('click', function() {
+            navbarCollapse.classList.remove('show');
+        });
+    });
+
+    // Cerrar el menú cuando se hace clic en cualquier otro enlace del menú
+    document.querySelectorAll('.nav-link:not(.dropdown-toggle)').forEach(function(link) {
+        link.addEventListener('click', function() {
+            if (navbarCollapse.classList.contains('show')) {
+                navbarCollapse.classList.remove('show');
+            }
+        });
+    });
+});
+
